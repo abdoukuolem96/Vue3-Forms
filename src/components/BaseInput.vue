6aadf91e8eb76872875dcd62874a44e4660ff9d7
kuolem96 <a.melouk@esi-sba.dz>
2022-01-07 19:53:45 +0100

    2- Base Input

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,26 @@
+<template>
+  <label>{{ label }}</label>
+  <input
+    :placeholder="label"
+    class="field"
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+    v-bind="$attrs"
+  />
+</template>
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: '',
+    },
+    modelValue: {
+      type: [String, Number],
+      default: '',
+    },
+  },
+}
+</script>
+
+<style></style>
